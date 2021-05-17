5a16a362ad96fe1b745052b6aea46c97cfcb5ef0
Firmanmsss <goma.neon@gmail.com>
2021-05-17 09:47:49 +0700

    ico baru

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,6 +5,14 @@
         </li>
         <li class=" nav-item"><a href="#"><i class="la la-television"></i><span class="menu-title" data-i18n="nav.templates.main">Inventory</span></a>
           <ul class="menu-content">
+            <li><a class="menu-item" href="#" data-i18n="nav.templates.horz.main">Customers</a>
+              <ul class="menu-content">
+                <li><a class="menu-item" href="{{ route('customer.index') }}" data-i18n="nav.templates.horz.classic">Data</a>
+                </li>
+                <li><a class="menu-item" href="{{ route('customer.create') }}" data-i18n="nav.templates.horz.top_icon">Add New</a>
+                </li>
+              </ul>
+            </li>
             <li><a class="menu-item" href="#" data-i18n="nav.templates.vert.main">Master Part Name</a>
               <ul class="menu-content">
                 <li><a class="menu-item" href="{{ route('partname.index') }}" data-i18n="nav.templates.vert.classic_menu">List Part Name</a>
@@ -16,14 +24,6 @@
                 <li><a class="menu-item" href="#" data-i18n="nav.templates.horz.classic">Transaksi</a>
                 </li>
                 <li><a class="menu-item" href="{{ route('goodreceipt.index') }}" data-i18n="nav.templates.horz.top_icon">Record</a>
-                </li>
-              </ul>
-            </li>
-            <li><a class="menu-item" href="#" data-i18n="nav.templates.horz.main">Customers</a>
-              <ul class="menu-content">
-                <li><a class="menu-item" href="{{ route('customer.index') }}" data-i18n="nav.templates.horz.classic">Data</a>
-                </li>
-                <li><a class="menu-item" href="{{ route('customer.create') }}" data-i18n="nav.templates.horz.top_icon">Add New</a>
                 </li>
               </ul>
             </li>
--- resources/views/templates/link.blade.php
+++ resources/views/templates/link.blade.php
@@ -1,5 +1,5 @@
   <link rel="apple-touch-icon" href="../../../app-assets/images/ico/apple-icon-120.png">
-  <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.ico">
+  <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/180_1.ico">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700" rel="stylesheet">
   <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/font-awesome-line-awesome/css/all.min.css">
