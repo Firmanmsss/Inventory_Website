c4582f31ee45d06b0f6b79fa30ded7a26063d713
Firmanmsss <goma.neon@gmail.com>
2021-05-18 18:10:33 +0700

    seeder category

--- database/migrations/2021_05_17_062955_create_categories_table.php
+++ database/migrations/2021_05_17_062955_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('category_name', 50);
+            $table->string('category_name', 255);
             $table->timestamps();
         });
     }
